499af42f8ea7a1dec12c2ba90a20afe2cace1f64
Joel Ward <jmward@gmail.com>
2017-03-31 11:43:32 -0700

    tighten up

--- e.py
+++ e.py
@@ -6,19 +6,29 @@ from g import G
 
 class Env:
     ARGS=[]
-    def __init__(_,B,C):
-        _.B, _.C, _.D, _.E = B, C, [], {}
-        pass
-    def LOOKUP(_,n):
-        #print "LOOKUP", n
-        try:
-            return _.E[n]
-        except KeyError:
-            if not _.B:
-                raise
-            return _.B.LOOKUP(n)
+    def __init__(_,B,C): _.B, _.C, _.D, _.E = B, C, [], {}
+    def e(_,_e): _.E = _e ; return _
+    def xLOOKUP(_,n):
+        x = _
+        ret = x.E.get(n)
+        while not ret:
+            x = x.B
+            ret = x.E.get(n)
+        return ret
+    def xSET(_,n,v):
+        x = _
+        ret = x.E.get(n)
+        while not ret:
+            if not x.B:
+                x = _.B
+                break
+            x = x.B
+            ret = x.E.get(n)
+            pass
+        x.E[n] = v
+        return _
     def step(_):
-        print "STEP", time.time(), repr((_.B, _.C, _.D, _.E))
+        #print "STEP", time.time(), repr((_.B, _.C, _.D, _.E))
         try:
             x = _.C.pop(0)
             _.D.append(int(x))
@@ -27,24 +37,21 @@ class Env:
                 _.B.D.extend(_.D)
             return _.B
         except TypeError: # a list
-            #print "TYP", type(x)
             return Env(_, x)
         except ValueError: # a string
             if x == "**":
-                Env.ARGS = _.D.pop(0)(*_.D)
-                #print "ARGS1 =", Env.ARGS
+                Env.ARGS = _.D.pop(0)(_, *_.D)
                 _.D = [Env.ARGS[0]]
             elif x == ";;":
                 _.D = []
             elif x[0] == '^':
-                _.D.append(_.LOOKUP(x[1:]))
+                _.D.append(_.xLOOKUP(x[1:]))
             elif x[0] == '$':
                 _.D.append(x[1:])
+            elif x[0] == '#':
+                print "SET TO", x
             else:
                 raise Exception()
-        return _
-    def e(_,_e):
-        _.E = _e
         return _
     def run(_):
         ovm = vm = _
@@ -70,4 +77,8 @@ print '='*80
 VM([["^prn","^x","**"], ";;",
     ["^prn","$yy","**"],";;",
     ["^prn","^y","**"]]).run()
+print '='*80
+VM([["#1:4","^set","$x","$qaz","**"], ";;",
+    ["^set","$v","$qaz2","**"], ";;",
+    ["^prn","^v","**"]]).run()
 print '='*80
--- g.py
+++ g.py
@@ -1,13 +1,21 @@
 
 
+import sys
+
+
 G = {}
 
 
-def prn(*args):
-    print "PRN", repr(args)
-    return [args[0],22,33]
+def prn(e,*args):
+    print " ====> PRN", repr(args)
+    return [list(args)]
 
 
+G['set'] = lambda e,k,v: [e.xSET(k,v)]
 G['prn'] = prn
+G['stdin' ] = sys.stdin
+G['stdout'] = sys.stdout
+G['stderr'] = sys.stderr
+G['readline'] = lambda e,f: f.readline()
 G['x'] = 109
 G['y'] = 208
